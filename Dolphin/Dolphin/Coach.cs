63be38b087d63493fae298172936ab95cea1b301
duckson1989 <andiolsen89@gmail.com>
2018-03-19 11:51:18 +0100

    Coach er done :-(
    CoachNum er done langsom men done

--- Dolphin/Dolphin/Coach.cs
+++ Dolphin/Dolphin/Coach.cs
@@ -8,5 +8,17 @@ namespace Dolphin
 {
     class Coach
     {
+        private string name;
+        private int coachNum;
+
+        public string Name { get{ return name; ; } private set{ name = value; }}
+        public int CoachNum { get{ return coachNum; } private set { coachNum = value; } }
+
+        public Coach(string name, int coachNum)
+        {
+            CoachNum = coachNum;
+            Name = name;
+        }
+
     }
 }
