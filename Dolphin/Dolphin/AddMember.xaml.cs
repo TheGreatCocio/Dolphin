c01e92038f04a356235be7e4d88fca4101554653
Mark Fog <TheBigCocio@outlook.dk>
2018-03-20 09:17:37 +0100

    UI

--- Dolphin/Dolphin/AddMember.xaml.cs
+++ Dolphin/Dolphin/AddMember.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Dolphin
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for AddMember.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AddMember : Window
     {
-        public MainWindow()
+        public AddMember()
         {
             InitializeComponent();
-        }
-
-        public void BtnNewMember_Click(object sender, RoutedEventArgs e)
-        {
-            AddMember addMember = new AddMember();
-            addMember.ShowDialog();
         }
     }
 }
---
+++ Dolphin/Dolphin/Model/MembershipType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolphin
+{
+    public class MembershipType
+    {
+        private string typeName;
+
+        public string TypeName { get => typeName; set => typeName = value; }
+
+        public MembershipType(string typeName)
+        {
+            TypeName = typeName;
+        }
+    }
+}
