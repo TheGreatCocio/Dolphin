49438911928af2d477d74d8ff684c1fb82f99fd8
Mark Fog <TheBigCocio@outlook.dk>
2018-03-20 09:34:30 +0100

    UI

--- Dolphin/Dolphin/Login.xaml.cs
+++ Dolphin/Dolphin/Login.xaml.cs
@@ -10,25 +10,25 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Dolphin
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Login.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Login : Window
     {
-        public MainWindow()
+        public Login()
         {
             InitializeComponent();
         }
 
-        public void BtnNewMember_Click(object sender, RoutedEventArgs e)
+        public void BtnLogin_Click(object sender, RoutedEventArgs e)
         {
-            AddMember addMember = new AddMember();
-            addMember.Show();
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
         }
     }
 }
